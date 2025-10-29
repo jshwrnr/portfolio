ac1c74593c81648091a2c84da522b85713dade5e
jshwrnr <joshwerner001@gmail.com>
2025-10-29 20:56:07 +0200

    Create photos page

---
+++ src/lib/polaroids.ts
@@ -0,0 +1,37 @@
+export const polaroids: Polaroid[] = [
+  {
+    src: "https://images.unsplash.com/photo-1571028634586-ae87c1a42432?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=686",
+    rotate: "-xs",
+    size: "sm",
+    label: {
+      text: "boat and birds",
+      class: "top-3 -rotate-6 text-center",
+    },
+  },
+  {
+    src: "https://images.unsplash.com/photo-1575362957017-4644306c8e4b?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDV8fGJsYWNrJTIwYW5kJTIwd2hpdGUlMjBhbmltYWx8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=500",
+    rotate: "md",
+    label: {
+      text: "cows on our trip to the highlands",
+      class: "top-2 rotate-1",
+    },
+  },
+  {
+    src: "https://plus.unsplash.com/premium_photo-1729092547788-371fe0b5b0cc?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1471",
+    rotate: "-sm",
+    size: "lg",
+    label: {
+      text: "it was a stormy day, birds flying, storm clouds rolling in, rumbling thunder starting",
+      class: "top-2 -rotate-3 text-center",
+    },
+  },
+  {
+    src: "https://images.unsplash.com/photo-1504019853082-9a4cb128c1ef?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=687",
+    rotate: "sm",
+    size: "sm",
+    label: {
+      text: "the brooklyn bridge is iconic!",
+      class: "top-2 -rotate-3",
+    },
+  },
+];
--- types.d.ts
+++ types.d.ts
@@ -7,3 +7,26 @@ interface Project {
   start: number | string;
   end?: number | string;
 }
+
+type PolaroidRotate =
+  | "-2xl"
+  | "-xl"
+  | "-lg"
+  | "-md"
+  | "-sm"
+  | "-xs"
+  | "none"
+  | "xs"
+  | "sm"
+  | "md"
+  | "lg"
+  | "xl"
+  | "2xl";
+
+interface Polaroid {
+  src: string;
+  rotate?: PolaroidRotate;
+  size?: "sm" | "md" | "lg";
+  label?: { text: string; class?: string };
+  pos?: { top?: number; left?: number; bottom?: number; right?: number };
+}
